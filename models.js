f5a62dc9b90da8815ce2bbf0aadadd3d452c5bb5
jithenderpara <parajithender@outlook.com>
2017-05-22 10:54:52 +0530

    node authentication
    node authentication

---
+++ models.js
@@ -0,0 +1,17 @@
+﻿var mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/Employee');
+
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+module.exports.User = mongoose.model('login', new Schema({
+    id: ObjectId,
+    firstName: { type: String, required: '{PATH} is required.' },
+    lastName: { type: String, required: '{PATH} is required.' },
+    email: { type: String, required: '{PATH} is required.', unique: true },
+    password: { type: String, required: '{PATH} is required.' },
+    data: Object,
+}));
+
+
+
---
+++ utils.js
@@ -0,0 +1,15 @@
+﻿var express = require("express");
+
+module.exports.createApp = function () {
+    var app = express();
+
+    //middlewires
+    app.use(require("./services/main"))
+    app.use(require("./services/auth"))
+
+    return app;
+}
+
+function sendViewTomiddlewire() {
+
+}

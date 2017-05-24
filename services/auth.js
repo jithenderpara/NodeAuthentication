293abc350c24691181536ac35fb3fb1c190d4b5e
jithenderpara <parajithender@outlook.com>
2017-05-24 17:26:42 +0530

    Updated
    Updated New

--- services/auth.js
+++ services/auth.js
@@ -1,59 +1,59 @@
 ﻿var express = require("express");
-//var models = require("../models");
-//var login = require("../models/login");
-var router = express.Router();
+var utils = require('../utils');
 var mongoose = require('mongoose');
-var modelschema = new mongoose.Schema({
-    email: {
-        type: String,
-        unique: true,
-        index: true
-    },
-    Password: { type: String, required: true }
-})
-var User = mongoose.model('logins', modelschema)
-//connect to mongoose
+//var models = require("../models");
+var login = require("../models/login");
+var userSession = require("../usersession");
+var router = express.Router();
 
-mongoose.connect('mongodb://localhost:27017/Newlogin');
+// To connect mongoDB
+mongoose.connect('mongodb://localhost/Newlogin');
 
-
-router.post('/register', function (req, res) {   
-    console.log(req.body)
-    var user = new User({
-        email: req.body.emailid,
-        Password: req.body.password
+router.post('/register', function (req, res) {
+    var user = new login.User({
+        email: req.body.email,
+        password: req.body.password,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
     })
     user.save(function (err) {
         if (err) {
             var error = 'Something bad happened! Please try again.';
-
             if (err.code === 11000) {
                 error = 'That email is already taken, please try another.';
             }
-
-            res.send(user);
+            res.send({ status: "Fail", msg: "That email is already taken, please try another.", url: "/login" });
         } else {
-            //utils.createUserSession(req, res, User);
-            res.sendView('/dashboard');
+            utils.createUserSession(req, res, user);
+            res.send({ status: user, msg: "Sucess Register", url: "/Dashboard" });
         }
     });
 });
-router.post('/login', function (req, res) {    
-    User.findOne({email: req.body.email }, function (err, user) {
+router.post('/login', function (req, res) {
+    login.User.findOne({ email: req.body.email }, function (err, user) {
         if (!user) {
-            res.send({ status: "OK", msg: "Invalid Email id", url: "/login" });
+            res.send({ status: "Fail", msg: "Invalid Email id", url: "/login" });
         } else {
-            console.log(req.body.password === user.password)
-            if (req.body.password == user.password) {
-                console.log(user)
-                //utils.createUserSession(req, res, user);
+            if (req.body.password == user.password) {              
+                utils.createUserSession(req, res, user);
                 res.send({ status: "OK", msg: "Sucess login", url: "/dashboard" });
             }
             else {
-                res.send({ status: "OK", msg: "Invalid Password", url: "/login" });
+                res.send({ status: "Fail", msg: "Invalid Password", url: "/login" });
             }
         }
     });
 });
+
+/**
+ * Log a user out of their account, then redirect them to the home page.
+ */
+router.get('/logout', function (req, res) {
+    if (req.session) {
+        req.session.reset();
+    }
+    res.redirect('/');
+});
+
 
 module.exports = router;

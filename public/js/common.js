74455e954390f61b5c13c6d1a1d7f01bc043a387
jithenderpara <parajithender@outlook.com>
2017-05-24 17:42:52 +0530

    updated
    updated

---
+++ public/js/common.js
@@ -0,0 +1,22 @@
+﻿function Ajaxcall(params, method) {
+    var resData;
+    $.ajax({
+        url: '/' + method,
+        type: 'POST',
+        data: params,
+        dataType: 'json',
+        async:false,
+        success: function (data) {
+            resData = data;
+        },
+        error: function (error, status, text) {
+            console.log("Error: " + JSON.stringify(error))
+            console.log("Status Code: " + status)
+            console.log("statusText: " + text)
+            alert("An error has occurred")
+            $('#info').html('<p>An error has occurred</p>');
+            resData = "Something went be wrong!!";
+        },        
+    });
+    return resData;
+}
---
+++ usersession.js
@@ -0,0 +1,18 @@
+﻿var express = require("express");
+var utils = require('./utils');
+
+
+
+
+
+/**
+ * Given a user object:
+ *
+ *  - Store the user object as a req.user
+ *  - Make the user object available to templates as #{user}
+ *  - Set a session cookie with the user object
+ *
+ *  @param {Object} req - The http request object.
+ *  @param {Object} res - The http response object.
+ *  @param {Object} user - A user object.
+ */
